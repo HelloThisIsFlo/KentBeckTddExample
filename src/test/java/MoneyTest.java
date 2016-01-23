1827de93e10e6479c0ddee9a208750e8b96307e1
Florian Kempenich <ShockN745@gmail.com>
2016-01-23 20:04:34 +0100

    Chapter 11

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Test for the Dollar and Franc classes
+ * Test for the Money classes
  */
-public class CurrencyTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() throws Exception {
@@ -16,34 +16,13 @@ public class CurrencyTest {
     }
 
     @Test
-    public void testFrancMultiplication() throws Exception {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testEquality() throws Exception {
         // Test equality for Dollar
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        // Test equality for Franc
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         // Test equality Dollar & Francs
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void testDifferentClassEquality() throws Exception {
-        // Test equality for different classes
-        String currency = "CHF";
-        assertTrue(new Money(5, currency).equals(new Franc(5, currency)));
-
-        currency = "USD";
-        assertTrue(new Money(5, currency).equals(new Dollar(5, currency)));
     }
 
     @Test
